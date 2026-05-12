4b2074698d573a144620224f4f644f04cbf1001d
Antuanete01 <u202310358@upc.edu.pe>
2026-05-12 13:39:21 -0500

    feat(IAM): first commit

---
+++ src/main/java/com/edifika/iam/EdifikaMicroserviceIamApplication.java
@@ -0,0 +1,13 @@
+package com.edifika.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdifikaMicroserviceIamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdifikaMicroserviceIamApplication.class, args);
+    }
+
+}
